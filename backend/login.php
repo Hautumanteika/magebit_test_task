34a2ba3e540ee9017b85576113e73f5eab88b977
Hautumanteika <55089583+Hautumanteika@users.noreply.github.com>
2020-09-14 14:31:41 +0300

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -4,14 +4,13 @@ include 'core/User.php';
 $user = new User();
 
 if(isset($_POST['Login'])) {
-    $useremail = $_POST["login_user_email"];
-    $userpass = $_POST["login_user_password"];
-    if(empty($useremail) || empty($userpass)) {
-        header("Error: 'one of fields is empty'");
-        header("Location: ../uncompleted.html");
+    $useremail = $_POST["login_user_email"];        //Get email
+    $userpass  = $_POST["login_user_password"];     //Ger password
+    if(empty($useremail) || empty($userpass)) {     //Check if not empty
+        header("Location: ../uncompleted.html");    //To error page
     }
     else {
-        $user -> login($useremail , $userpass);
+        $user -> login($useremail , $userpass);     //Send data to user class
     }
 }
 ?>
