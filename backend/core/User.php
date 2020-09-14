6fa98434c10b9d51f6f5b6fb70e5fb834ae1ed82
Hautumanteika <55089583+Hautumanteika@users.noreply.github.com>
2020-09-14 14:41:26 +0300

    Update User.php

--- backend/core/User.php
+++ backend/core/User.php
@@ -7,34 +7,34 @@ class User
     }
 
     public function register($name , $email , $password) {
-        $db = new Database();
-        $check = $db -> connection -> query("SELECT * FROM registred_users WHERE email = '$email'");
-        if(mysqli_num_rows($check) == 0) {
+        $db    = new Database();
+        $check = $db -> connection -> query("SELECT * FROM registred_users WHERE email = '$email'");                        //Check if user exists
+        if(mysqli_num_rows($check) == 0) {                                                                                  //If user not exists
         $encrypted_password = password_hash($password,  PASSWORD_DEFAULT);
-        $sql = "INSERT INTO registred_users (name , email , password) values ('$name' , '$email' , '$encrypted_password')";
-        $input = mysqli_query($db -> connection,$sql);
+        $sql = "INSERT INTO registred_users (name , email , password) values ('$name' , '$email' , '$encrypted_password')"; //Insert into database
+        $input = mysqli_query($db -> connection,$sql);                                                                      //Input to database
         if ($input === TRUE) {
-            header("Location: ../completed.html");
+            header("Location: ../completed.html");                                                                          //To completed page
         } else {
-            header("Location: ../uncompleted.html");
+            header("Location: ../uncompleted.html");                                                                        //To uncompleted page
         }
         }
         else {
-            header("Location: ../uncompleted.html");
+            header("Location: ../uncompleted.html");                                                                        //To uncompleted page
         }
     }
 
     public function login($email , $password) {
-        $db = new Database();
-        $sql = $db -> connection -> query("SELECT * FROM registred_users WHERE email = '$email' LIMIT 1");
+        $db  = new Database();
+        $sql = $db -> connection -> query("SELECT * FROM registred_users WHERE email = '$email' LIMIT 1");                  //Search if user exists
         $row = mysqli_fetch_array($sql, MYSQLI_ASSOC);
-        if($verify = password_verify($password, $row['password'])) {
-            $_SESSION['logged_user_name'] = $row['name'];
-            $_SESSION['logged_user_email'] = $row['email'];
-            header("Location: ../profile.html");
+        if($verify = password_verify($password, $row['password'])) {                                                        //Check if input password = hashed password in database
+            $_SESSION['logged_user_name']  = $row['name'];                                                                  //Set logged_user_name
+            $_SESSION['logged_user_email'] = $row['email'];                                                                 //Set logged_user_email
+            header("Location: ../profile.html");                                                                            //To profile
         }
         else {
-            header("Location: ../uncompleted.html");
+            header("Location: ../uncompleted.html");                                                                        //To uncompleted notification page
         }
     }
 
