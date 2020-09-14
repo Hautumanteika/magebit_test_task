11c6cde63f7b161aae32ad264760262add8709e8
Hautumanteika <55089583+Hautumanteika@users.noreply.github.com>
2020-09-14 14:36:33 +0300

    Update Database.php

--- backend/core/Database.php
+++ backend/core/Database.php
@@ -6,20 +6,20 @@ class Database {
     public $connection;
 
     public function __construct() {
-        $this->connection = mysqli_connect(hostname, user, password, database);
-        $this->connect();
+        $this->connection = mysqli_connect(hostname, user, password, database); //Connect with database
+        $this->connect();                                                       //Call connect function
         if (!$this->connection)
-            die("Connection failed: " . mysqli_connect_error());
+            die("Connection failed: " . mysqli_connect_error());                //Error if not coneccted
     }
 
-    public function connect() {
+    public function connect() {                                                 //Create table if not exists
         $tbl_users = 'CREATE TABLE IF NOT EXISTS registred_users (
             id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(64) NOT NULL,
             email VARCHAR(30) NOT NULL,
             password VARCHAR(256) NOT NULL
           )';
-        mysqli_query($this -> connection,$tbl_users);
+        mysqli_query($this -> connection,$tbl_users);                            //Insert to database users
     }
 }
 
