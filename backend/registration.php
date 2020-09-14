ba7e0095baffea30d93ecf7518b19d00ead40f86
Hautumanteika <55089583+Hautumanteika@users.noreply.github.com>
2020-09-14 14:29:57 +0300

    Update registration.php

--- backend/registration.php
+++ backend/registration.php
@@ -4,14 +4,14 @@ include 'core/User.php';
 $user = new User();
 
 if(isset($_POST["Sign_up"])) {
-    $username = $_POST['user_name'];
-    $useremail = $_POST['user_email'];
-    $userpass = $_POST['user_password'];
-    if(empty($username) || empty($useremail) || empty($userpass)) {
+    $username  = $_POST['user_name'];                               //Get name
+    $useremail = $_POST['user_email'];                              //Get email
+    $userpass  = $_POST['user_password'];                           //Get password
+    if(empty($username) || empty($useremail) || empty($userpass)) { //Check if inputs aren't empty
         header("Location: ../uncompleted.html");
     }
     else {
-        $user -> register($username , $useremail , $userpass);
+        $user -> register($username , $useremail , $userpass);       //Send data to User class
     }
 }
 ?>
